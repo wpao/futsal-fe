b6f51c376585118ceffb4bd6a7ce3c7f98bac7cf
AXHAUFI <xhaufinonstop@gmail.com>
2025-01-19 21:31:24 +0700

    v1.0.0 merapikan ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import CheckPage from "./pages/CheckPage";
 import InfoPage from "./pages/InfoPage";
 import { Header } from "./components/Header";
-import EditJam from "./pages/admin/EditJam";
+import JamPage from "./pages/admin/JamPage";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <Route path="/check" Component={CheckPage} />
         <Route path="/info" Component={InfoPage} />
         <Route path="/admin">
-          <Route path="edit/" Component={EditJam} />
-          <Route path="810009/:jamId" Component={EditJam} />
+          <Route path="edit/" Component={JamPage} />
+          <Route path="810009/:jamId" Component={JamPage} />
         </Route>
       </Routes>
       <Toaster />
