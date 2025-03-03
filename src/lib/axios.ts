325e525babbf577b5754b488d78490aee58a2269
wpao <wpaozan@gmail.com>
2025-03-03 13:44:43 +0700

    v1.0.1 run on docker

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:2000",
   // baseURL: "https://futsal.authenticrinjani.com",
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  // baseURL: "http://futsal-backend:3000",
+  baseURL: "/api", // Gunakan proxy di Nginx
 });
 
 // Interceptor untuk menambahkan token JWT ke header
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -7,6 +20,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://futsal-backend:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 })
